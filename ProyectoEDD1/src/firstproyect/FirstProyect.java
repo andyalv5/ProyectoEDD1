96c68e4e105d2114ad2550f7caf3e1ee0d5b4b91
Andy Alvarez <andynet@iMac-de-Andy-alv.local>
2021-05-11 17:47:30 -0400

    El inicio de la interfaz

--- ProyectoEDD1/src/firstproyect/FirstProyect.java
+++ ProyectoEDD1/src/firstproyect/FirstProyect.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectoedd1;
+package firstproyect;
 
 /**
  *
- * @author andy
+ * @author andynet
  */
-public class ProyectoEDD1 {
+public class FirstProyect {
 
     /**
      * @param args the command line arguments

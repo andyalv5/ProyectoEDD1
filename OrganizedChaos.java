aad9d2a7b718e532d840f32466ec48ecca2bdaa0
ghectorg <84487270+ghectorg@users.noreply.github.com>
2021-05-21 17:48:28 -0400

    prueba a ver queloque
    scvsefwfwseww

---
+++ OrganizedChaos.java
@@ -0,0 +1,17 @@
+package organizedchaos;
+
+public class OrganizedChaos {
+
+    public static void main(String[] args) {
+        
+        Funciones fc = new Funciones();
+        
+        ListaSimple prueba = fc.Leer_txt("test\\Archivo.txt");
+        
+        System.out.println(prueba.printList());
+        /*Interfaz ventana = new Interfaz();
+        
+        ventana.setVisible(true);*/
+    }
+    
+}
